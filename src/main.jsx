893072d9e060a9b8258c613248eecc41e5154321
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-03-17 12:32:02 -0500

    08 creando cascaron para todos

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,14 @@ import ReactDOM from 'react-dom/client'
 //import { Memorize } from './06-memos/Memorize'
 //import { MemoHook } from './06-memos/MemoHook'
 //import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
-
-import './08-useReducer/intro-reducer'
+//import { Padre } from './07-tarea-memo/Padre'
+//import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
- //   <Padre/>
+  <div className='container'>
+    <TodoApp/>
+  </div>
   //</React.StrictMode>,
 )
