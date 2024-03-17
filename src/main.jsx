db85d239757501bc299c6842979ecc2294f39c71
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-03-17 11:51:31 -0500

    08 introduccion del reducer

--- src/main.jsx
+++ src/main.jsx
@@ -13,9 +13,10 @@ import ReactDOM from 'react-dom/client'
 //import { CallbackHook } from './06-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Padre/>
+ //   <Padre/>
   //</React.StrictMode>,
 )
