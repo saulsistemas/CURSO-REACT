301db5837e581ddac58b96f38139aabf8b3dbdfb
Sandro Santamaria Ramos <R000001@inmobiliariahuertas.local>
2024-03-18 11:52:05 -0500

    NavLink

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/about">About</Link>
-        </>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded-3">
+            <div className='container-fluid'>
+                <Link className="navbar-brand" to="/"> useContext </Link>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="/"
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="/login"
+                        >
+                            Login
+                        </NavLink>
+                        <NavLink
+                            className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+                            to="/About"
+                        >
+                            About
+                        </NavLink>
+                        {/* {<Link className="nav-item nav-link active" to="/">Home </Link>
+                    <Link className="nav-item nav-link" to="/login">Login</Link>
+                    <Link className="nav-item nav-link" to="/about">About</Link>} */}
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+
     )
 }
